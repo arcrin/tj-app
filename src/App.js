25fd7a5750a981dbdef367752e7a55b7f4e4e6ab
wbai <wbai@deltacontrols.com>
2019-01-17 17:00:23 -0800

    from work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className={"main"}>
+          <div className={"action"}>Action Bar</div>
+          <div className={"tj"}>Test Jig</div>
+          <div className={"ts"}>Test Suite</div>
+          <div className={"tc"}>Test Case</div>
+          <div className={"product"}>Product</div>
+        </div>
+        <div className={"jig_list"}>
+          <div className={"test_jig"}>TJ1</div>
+          <div className={"test_jig"}>TJ2</div>
+          <div className={"test_jig"}>TJ3</div>
+          <div className={"test_jig"}>TJ4</div>
+          <div className={"test_jig"}>TJ5</div>
+          <div className={"test_jig"}>TJ5</div>
+          <div className={"test_jig"}>TJ5</div>
+          <div className={"test_jig"}>TJ5</div>
+          <div className={"test_jig"}>TJ5</div>
+          <div className={"test_jig"}>TJ5</div>
+          <div className={"test_jig"}>TJ5</div>
+        </div>
       </div>
     );
   }
